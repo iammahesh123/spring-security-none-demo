3ab9676341c3c70f3345995729577cb3ce23165d
iammahesh123 <maheshkadambala18@gmail.com>
2024-03-07 13:42:20 +0530

    spring security none demo

---
+++ src/main/java/com/gfg/securitynonedemo/SecurityNoneDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gfg.securitynonedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityNoneDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityNoneDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gfg/securitynonedemo/SecurityNoneDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfg.securitynonedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityNoneDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
